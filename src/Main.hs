04cb5f369390034685dba4fa38cad005d4fd5531
葉岬 <XGLey@protonmail.com>
2018-09-09 14:30:25 +0800

    再次分离路由

--- src/Main.hs
+++ src/Main.hs
@@ -1,45 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend, (<>))
-import           Hakyll
+import Hakyll
 import XG.Type
+import XG.Route
 import Config
-
-postPattern :: Pattern
-postPattern = fromGlob $ sitePostDir config <> "/*"
-
-copyProcess :: Rules ()
-copyProcess = route idRoute >> compile copyFileCompiler
-
-imageRoute :: Rules ()
-imageRoute = match "image/**" copyProcess
-
-cssRoute :: Pattern -> Rules()
-cssRoute pat = match pat copyProcess
-
-postRoute :: Pattern -> Rules ()
-postRoute pat = match pat $ do
-    route $ setExtension "html"
-    compile $ do
-        pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html" postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-indexRoute :: Rules ()
-indexRoute = match "index.html" $ do
-    route idRoute
-    compile $ do
-        posts <- recentFirst =<< loadAll postPattern
-        let ctx = listField "posts" postCtx (return posts)
-                  <> constField "title" "首页"
-                  <> defaultContext
-        getResourceBody
-            >>= applyAsTemplate ctx
-            >>= loadAndApplyTemplate "templates/default.html" ctx
-            >>= relativizeUrls
-
-templateRoute :: Rules ()
-templateRoute = match "templates/*" $ compile templateBodyCompiler
 
 main :: IO ()
 main = do
@@ -50,9 +13,3 @@ main = do
         postRoute postPattern
         indexRoute
         templateRoute
-
-
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
---
+++ src/XG/Route.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+module XG.Route where
+
+import Hakyll
+
+import Data.Monoid ((<>))
+
+import XG.Type
+import Config
+
+postPattern :: Pattern
+postPattern = fromGlob $ sitePostDir config <> "/*"
+
+copyProcess :: Rules ()
+copyProcess = route idRoute >> compile copyFileCompiler
+
+imageRoute :: Rules ()
+imageRoute = match "image/**" copyProcess
+
+cssRoute :: Pattern -> Rules()
+cssRoute pat = match pat copyProcess
+
+postRoute :: Pattern -> Rules ()
+postRoute pat = match pat $ do
+    route $ setExtension "html"
+    compile $ do
+        pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+indexRoute :: Rules ()
+indexRoute = match "index.html" $ do
+    route idRoute
+    compile $ do
+        posts <- recentFirst =<< loadAll postPattern
+        let ctx = listField "posts" postCtx (return posts)
+                  <> constField "title" "首页"
+                  <> defaultContext
+        getResourceBody
+            >>= applyAsTemplate ctx
+            >>= loadAndApplyTemplate "templates/default.html" ctx
+            >>= relativizeUrls
+
+templateRoute :: Rules ()
+templateRoute = match "templates/*" $ compile templateBodyCompiler
+
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
