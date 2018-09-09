dee28e33fd514a8da925092be879ba4dd99953e5
葉岬 <XGLey@protonmail.com>
2018-09-09 15:12:58 +0800

    分离后的基本骨架

--- src/XG/Route.hs
+++ src/XG/Route.hs
@@ -4,12 +4,13 @@ module XG.Route where
 import Hakyll
 
 import Data.Monoid ((<>))
+import Control.Monad ((>=>))
 
 import XG.Type
 import Config
 
 postPattern :: Pattern
-postPattern = fromGlob $ sitePostDir config <> "/*"
+postPattern = fromGlob $ sitePostDir config <> "/**.org"
 
 copyProcess :: Rules ()
 copyProcess = route idRoute >> compile copyFileCompiler
@@ -18,34 +19,36 @@ imageRoute :: Rules ()
 imageRoute = match "image/**" copyProcess
 
 cssRoute :: Pattern -> Rules()
-cssRoute pat = match pat copyProcess
+cssRoute pat = match pat $ do
+    route idRoute
+    compile compressCssCompiler
 
 postRoute :: Pattern -> Rules ()
 postRoute pat = match pat $ do
     route $ setExtension "html"
     compile $ do
         pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html" postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+            >>= loadAndApplyTemplate "tpl/wfvh.html" defaultContext
+            >>= applyLayout defaultContext
 
 indexRoute :: Rules ()
-indexRoute = match "index.html" $ do
+indexRoute = create ["index.html"] $ do
     route idRoute
     compile $ do
         posts <- recentFirst =<< loadAll postPattern
         let ctx = listField "posts" postCtx (return posts)
                   <> constField "title" "首页"
                   <> defaultContext
-        getResourceBody
-            >>= applyAsTemplate ctx
-            >>= loadAndApplyTemplate "templates/default.html" ctx
-            >>= relativizeUrls
+        makeItem ""
+            >>= loadAndApplyTemplate "tpl/index.html" ctx
+            >>= applyLayout ctx
 
 templateRoute :: Rules ()
-templateRoute = match "templates/*" $ compile templateBodyCompiler
+templateRoute = match "tpl/*" $ compile templateBodyCompiler
+
+-- | 载入模板
+applyLayout :: Context a -> Item a -> Compiler (Item String)
+applyLayout ctx = loadAndApplyTemplate "tpl/layout.html" ctx >=> relativizeUrls
 
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" <> defaultContext
