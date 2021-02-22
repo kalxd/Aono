5078548c94b1b4d1349b8d0d99e0e898c42e80c0
荀徒之 <kalxd@126.com>
2021-02-22 13:22:54 +0800

    设置编码，虽然输出是乱码，但生成文件都正常。

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,10 @@
+import qualified GHC.IO.Encoding as E
+
 import Aono.Option.App (runAppWith, readAppConfig)
 import Aono.Route (runRoute)
 
 main :: IO ()
 main = do
+    E.setLocaleEncoding E.utf8 -- 设置编码环境，在windows上尤为重要。
     config <- readAppConfig
     runAppWith config $ runRoute config
