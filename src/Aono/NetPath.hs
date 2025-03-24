32dbb52188b2c7d1351cf8dd5b5704d292b94219
荀洪道 <kalxd@126.com>
2025-03-24 10:39:31 +0800

    NetPath去除RIO依赖。

--- src/Aono/NetPath.hs
+++ src/Aono/NetPath.hs
@@ -1,19 +1,17 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 -- | 网络路径
 module Aono.NetPath where
 
-import RIO
 import Data.List (intercalate, dropWhileEnd)
-import RIO.FilePath (splitPath, pathSeparator, joinPath, (</>))
+import System.FilePath (pathSeparator, joinPath, (</>), splitPath)
 
 newtype NetPath = NetPath { runNetPath :: [String] }
     deriving (Show)
 
 instance Semigroup NetPath where
-  (<>) (NetPath p1) (NetPath p2) = NetPath $ p1 <> p2
+    (<>) (NetPath p1) (NetPath p2) = NetPath $ p1 <> p2
 
 instance Monoid NetPath where
-  mempty = NetPath mempty
+    mempty = NetPath mempty
 
 pathToNetPath :: FilePath -> NetPath
 pathToNetPath = NetPath . map (dropWhileEnd (== pathSeparator)) . splitPath
